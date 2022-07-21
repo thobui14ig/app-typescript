d8572b21dae671b10126097dfd37dceb31e19a9f
thobui1996 <buithanhtho20ig@gmail.com>
2022-07-21 23:21:03 +0700

    up

--- src/pages/AppNhaHang/Thongke/index.js
+++ src/pages/AppNhaHang/Thongke/index.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import SelectBox from 'devextreme-react/select-box';
-import './thongke.css'
-import { getDoanhthuthang, getDoanhthutuan } from '../../../api/methods/thongke.method';
+import DateBox from 'devextreme-react/date-box';
+import './thongke.scss'
+import { getDoanhthunam, getDoanhthuthang, getDoanhthutuan } from '../../../api/methods/thongke.method';
 import dayjs from 'dayjs';
 
 const monthArr = [1,2,3,4,5,6,7,8,9,10,11,12]
@@ -11,15 +12,36 @@ const curentMonth = day.getMonth() + 1;
 const startWeek = dayjs(day).startOf('week').format('YYYY-MM-DD')
 const endWeek = dayjs(day).endOf('week').format('YYYY-MM-DD')
 
+const year = (new Date()).getFullYear();
+const listYearPlus = Array.from(new Array(5),(val, index) => index + year);
+const listYearSub = Array.from(new Array(5),(val, index) => year - index  );
+let listYear = [...new Set(listYearSub.concat(listYearPlus))].sort();
+
 function Thongke() {
   const [month, setMonth] = useState(curentMonth)
+  const [currentYear, setCurrentYear] = useState(year)
+
   const [doanhthuthang, setDoanhthuthang] = useState(0)
   const [doanhthutuan, setDoanhthutuan] = useState(0)
+  const [doanhthunam, setDoanhthunam] = useState(0)
 
 
-  const onChange = (e) => {
+
+  const onChangeMonth = (e) => {
     setMonth(e.value);
   }
+  const onChangeYear = (e) => {
+    setCurrentYear(e.value);
+  }
+  useEffect(() => {
+    const fetch = async() => {
+        const data = await getDoanhthunam(currentYear)
+        setDoanhthunam(data)
+    }
+
+    fetch();
+  }, [currentYear])
+
   useEffect(() => {
     const fetch = async() => {
         const data = await getDoanhthuthang(month)
@@ -37,13 +59,12 @@ function Thongke() {
     fetch();
   }, [])
 
-
   return (
-    <div className="flex-container">
+    <div className="flex-container thongke">
       <div>
         <p>Doanh thu tháng {month}:</p>
         <div>
-          <SelectBox items={monthArr} onValueChanged={onChange}/>
+          <SelectBox items={monthArr} onValueChanged={onChangeMonth} value={month}/>
         </div>
         <p>{doanhthuthang.toLocaleString('it-IT', {style : 'currency', currency : 'VND'})}</p>
       </div>
@@ -53,7 +74,10 @@ function Thongke() {
       </div>
       <div>
         <p>Doanh thu năm:</p>
-        <p>{(doanhthutuan).toLocaleString('it-IT', {style : 'currency', currency : 'VND'})}</p>
+        <div>
+          <SelectBox items={listYear}  onValueChanged={onChangeYear} value={currentYear}/>
+        </div>
+        <p>{(doanhthunam).toLocaleString('it-IT', {style : 'currency', currency : 'VND'})}</p>
       </div>
     </div>
   )
